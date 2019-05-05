d9b291fae781a072c43f1a253b3843ae962aa043
Ryan Antkowiak <antkowiak@gmail.com>
2019-05-05 09:37:44 -0500

    Update init_w10.ps1

--- powershell/init_w10.ps1
+++ powershell/init_w10.ps1
@@ -48,3 +48,18 @@ if (!(Get-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\CloudContent" -E
 }
 Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\CloudContent" -Name "DisableWindowsConsumerFeatures" -Value 1 -Type DWord -Force
 
+# Uninstall and disable One Drive
+taskkill /f /im OneDrive.exe
+%SystemRoot%\System32\OneDriveSetup.exe /uninstall
+%SystemRoot%\SysWOW64\OneDriveSetup.exe /uninstall
+rd "%UserProfile%\OneDrive" /Q /S
+rd "%LocalAppData%\Microsoft\OneDrive" /Q /S
+rd "%ProgramData%\Microsoft OneDrive" /Q /S
+rd "C:\OneDriveTemp" /Q /S
+REG Delete "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
+REG Delete "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
+if (!(Get-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\OneDrive" -ErrorAction SilentlyContinue))
+{
+  New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\OneDrive"
+}
+Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\OneDrive" -Name "DisableFileSyncNGSC" -Value 1 -Type DWord -Force
