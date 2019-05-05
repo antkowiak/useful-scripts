1ca5c62f2fadbf5391c007c64ad553a225955ccf
Ryan Antkowiak <antkowiak@gmail.com>
2019-05-05 10:35:36 -0500

    Update init_w10.ps1

--- powershell/init_w10.ps1
+++ powershell/init_w10.ps1
@@ -90,6 +90,6 @@ Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\OneDrive" -Nam
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Value 1 -Type DWord -Force
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Value 1 -Type DWord -Force
 
-
-
+# Disable automatic reboot on BSOD
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\CrashControl" -Name "AutoReboot" -Value 0 -Type DWord -Force
 
