4622f13f5b61c6a917bc2e8916f65485ea05789c
Ryan Antkowiak <antkowiak@gmail.com>
2019-05-05 08:34:28 -0500

    Update init_w10.ps1

--- powershell/init_w10.ps1
+++ powershell/init_w10.ps1
@@ -1,5 +1,7 @@
 # Initialize my common preferences on a Windows 10 installation
 
+# Set PowerShell execution policy to be able to make registry changes
+Set-ExecutionPolicy RemoteSigned
+
 # Disable startup delay
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\Serialize" -Name "StartupDelayInMSec" -Value 0 -Type DWord
-
