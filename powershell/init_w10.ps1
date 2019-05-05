7966b0f9a338cec7c93aecd812047fcb679d45aa
Ryan Antkowiak <antkowiak@gmail.com>
2019-05-05 08:59:18 -0500

    Update init_w10.ps1

--- powershell/init_w10.ps1
+++ powershell/init_w10.ps1
@@ -12,3 +12,14 @@ Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer
 
 # Show "My Computer" on the desktop
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" -Value 0 -Type DWord -Force
+
+# Force Icon View in Control Panel
+if (!(Get-Item -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies" -ErrorAction SilentlyContinue))
+{
+  New-Item -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies"
+}
+if (!(Get-Item -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -ErrorAction SilentlyContinue))
+{
+  New-Item -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+}
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "ForceClassicControlPanel" -Value 1 -Type DWord -Force
