eb391fb4afc139bafb8d5be0bdfa0c4923fce0f0
Ryan Antkowiak <antkowiak@gmail.com>
2019-05-05 08:38:10 -0500

    Update init_w10.ps1

--- powershell/init_w10.ps1
+++ powershell/init_w10.ps1
@@ -4,4 +4,6 @@
 Set-ExecutionPolicy RemoteSigned
 
 # Disable startup delay
+New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\Serialize" -Name "StartupDelayInMSec" -Value 0 -Type DWord -Force
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\Serialize" -Name "StartupDelayInMSec" -Value 0 -Type DWord -Force
+
