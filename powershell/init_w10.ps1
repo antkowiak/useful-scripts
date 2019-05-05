8a11f4614ff0a22df94814edfb93bb44861e9e0b
Ryan Antkowiak <antkowiak@gmail.com>
2019-05-05 10:19:30 -0500

    Update init_w10.ps1

--- powershell/init_w10.ps1
+++ powershell/init_w10.ps1
@@ -58,10 +58,10 @@ if (Test-Path -Path "$Env:SystemRoot\SysWOW64\OneDriveSetup.exe" -PathType Leaf)
 {
   "$Env:SystemRoot\SysWOW64\OneDriveSetup.exe /uninstall"
 }
-RD /S $Env:UserProfile\OneDrive
-RD /S Remove-Item $Env:LocalAppData\Microsoft\OneDrive
-RD /S $Env:ProgramData\Microsoft\OneDrive
-RD /S C:\OneDriveTemp
+Remove-Item $Env:UserProfile\OneDrive -Force
+Remove-Item Remove-Item $Env:LocalAppData\Microsoft\OneDrive -Force
+Remove-Item $Env:ProgramData\Microsoft\OneDrive -Force
+Remove-Item C:\OneDriveTemp -Force
 REG Delete "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
 REG Delete "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
 if (!(Get-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\OneDrive" -ErrorAction SilentlyContinue))
