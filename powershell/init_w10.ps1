9979e6dd16c76ca818015ad0a8f84433dee4f4d0
Ryan Antkowiak <antkowiak@gmail.com>
2019-05-05 08:18:53 -0500

    Update init_w10.ps1

--- powershell/init_w10.ps1
+++ powershell/init_w10.ps1
@@ -1,6 +1,5 @@
 # Initialize my common preferences on a Windows 10 installation
 
-
 # Disable startup delay
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\Serialize" -Name "Startupdelayinmsec" -Value 0 -Type DWord
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\Serialize" -Name "StartupDelayInMSec" -Value 0 -Type DWord
 
