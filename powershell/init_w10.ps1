ccc777a6cf0a8a6e3e293390c15448620d1176dc
Ryan Antkowiak <antkowiak@gmail.com>
2019-05-05 09:40:25 -0500

    Update init_w10.ps1

--- powershell/init_w10.ps1
+++ powershell/init_w10.ps1
@@ -50,11 +50,11 @@ Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\CloudContent" 
 
 # Uninstall and disable One Drive
 taskkill /f /im OneDrive.exe
-%SystemRoot%\System32\OneDriveSetup.exe /uninstall
-%SystemRoot%\SysWOW64\OneDriveSetup.exe /uninstall
-rd "%UserProfile%\OneDrive" /Q /S
-rd "%LocalAppData%\Microsoft\OneDrive" /Q /S
-rd "%ProgramData%\Microsoft OneDrive" /Q /S
+$env:SystemRoot\System32\OneDriveSetup.exe /uninstall
+$env:SystemRoot\SysWOW64\OneDriveSetup.exe /uninstall
+rd "$env:UserProfile\OneDrive" /Q /S
+rd "$env:LocalAppData\Microsoft\OneDrive" /Q /S
+rd "$env:ProgramData\Microsoft OneDrive" /Q /S
 rd "C:\OneDriveTemp" /Q /S
 REG Delete "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
 REG Delete "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
