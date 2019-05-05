375794e4b0a7abb5ef462b66a73d05ce1af2561b
Ryan Antkowiak <antkowiak@gmail.com>
2019-05-05 09:17:00 -0500

    Update init_w10.ps1

--- powershell/init_w10.ps1
+++ powershell/init_w10.ps1
@@ -14,18 +14,18 @@ Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" -Value 0 -Type DWord -Force
 
 # Force Icon View in Control Panel
-if (!(Get-Item -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies" -ErrorAction SilentlyContinue))
+if (!(Get-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies" -ErrorAction SilentlyContinue))
 {
-  New-Item -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies"
+  New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies"
 }
-if (!(Get-Item -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -ErrorAction SilentlyContinue))
+if (!(Get-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -ErrorAction SilentlyContinue))
 {
-  New-Item -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+  New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
 }
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "ForceClassicControlPanel" -Value 1 -Type DWord -Force
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "ForceClassicControlPanel" -Value 1 -Type DWord -Force
 
 # Always show file extensions
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Value 0 -Type DWord -Force
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Value 0 -Type DWord -Force
 
 # Always show all files
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowSuperHidden" -Value 1 -Type DWord -Force
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowSuperHidden" -Value 1 -Type DWord -Force
