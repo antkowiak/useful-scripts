c250c555e0cb2d54d59f9c13b2cdd2f914a10e2f
Ryan Antkowiak <antkowiak@gmail.com>
2022-05-07 07:56:21 -0500

    Add files via upload

--- bash/upgrade.sh
+++ bash/upgrade.sh
@@ -13,38 +13,60 @@
 # Created by Ryan Antkowiak (antkowiak@gmail.com)
 #
 
+COLOR_INVERSE="\e[7m"
+COLOR_DEFAULT="\e[0m"
+
+UPGRADE_LIST=""
 
 if [[ $EUID -ne 0 ]];
 then
     exec sudo /bin/bash "$0" "$@"
 fi
 
+function print_upgrade_list()
+{
+    if [[ ! -z "${UPGRADE_LIST}" ]]
+    then
+        echo -e "Upgrade List: ${COLOR_INVERSE}${UPGRADE_LIST}${COLOR_DEFAULT}"
+    fi
+}
+
 function check_reboot_needed()
 {
     if [ -f /var/run/reboot-required ]
         then
-        echo "Reboot Required"
+        print_upgrade_list
+        echo -e "${COLOR_INVERSE}Reboot Required${COLOR_DEFAULT}"
         exit
     fi
 }
 
+
 check_reboot_needed
 apt -y update
+if [ $? -ne 0 ] ; then { echo -e "${COLOR_INVERSE}Failure during: 'apt -y update' - Exiting...${COLOR_DEFAULT}" ; exit 1; } fi
+
+UPGRADE_LIST=`apt list --upgradable 2>/dev/null |grep -v "Listing..." |cut -f1 -d'/' |tr '\n' ' '  |sed 's/ *$//'`
 
 check_reboot_needed
 apt -y upgrade
+if [ $? -ne 0 ] ; then { echo -e "${COLOR_INVERSE}Failure during: 'apt -y upgrade' - Exiting...${COLOR_DEFAULT}" ; exit 1; } fi
 
 check_reboot_needed
 apt -y dist-upgrade
+if [ $? -ne 0 ] ; then { echo -e "${COLOR_INVERSE}Failure during: 'apt -y dist-upgrade' - Exiting...${COLOR_DEFAULT}" ; exit 1; } fi
 
 check_reboot_needed
 apt -y autoremove
+if [ $? -ne 0 ] ; then { echo -e "${COLOR_INVERSE}Failure during: 'apt -y autoremove' - Exiting...${COLOR_DEFAULT}" ; exit 1; } fi
 
 check_reboot_needed
 apt -y clean
+if [ $? -ne 0 ] ; then { echo -e "${COLOR_INVERSE}Failure during: 'apt -y clean' - Exiting...${COLOR_DEFAULT}" ; exit 1; } fi
 
 check_reboot_needed
 
+print_upgrade_list
 lsb_release -a
 uname -a
 
